ab56bd89eb798c8c7f26a14a53c18d6230c75de6
chengzhou <1738248438@qq.com>
2026-05-27 17:00:03 +0800

    chore: 删除CHANGELOG和commitlint配置及prepare脚本
    - 移除server模块中的CHANGELOG.md文件
    - 删除commitlint.config.cjs文件，取消提交消息格式校验配置
    - 删除prepare.js脚本，去除husky安装命令
    - 精简项目配置，减少无用文件和脚本

--- server/scripts/prepare.js
+++
@@ -1,9 +0,0 @@
-import { execSync } from 'child_process'
-
-const isProduction = process.env.NODE_ENV === 'production'
-
-if (isProduction) {
-  process.exit(0)
-}
-
-execSync('husky install', { stdio: 'inherit' })
